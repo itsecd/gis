dfc4245be696cd53b9a1edcdd5e759cc5aed4478
kolyanch <kolyanch@outlook.com>
2023-04-29 18:11:59 +0400

    Анимация

--- client/src/lib/Map.svelte
+++ client/src/lib/Map.svelte
@@ -1,31 +1,24 @@
 <script lang="ts">
-    import maplibregl, {BoxZoomHandler, MouseRotateHandler, type MapLibreEvent, RasterBoundsArray } from 'maplibre-gl';
+    import maplibregl, {BoxZoomHandler, MouseRotateHandler, type MapLibreEvent, RasterBoundsArray, GeoJSONSource, GeoJSONFeature } from 'maplibre-gl';
     import { onMount } from 'svelte';
     import 'maplibre-gl/dist/maplibre-gl.css';
 
     let map: maplibregl.Map;
 
-    function addGeoJsonLayer(id, data, source, type) {
-        map.addSource(source, {
-          type: "geojson",
-          data: data
-        })
-        map.addLayer(
-        {
-            'id' : id,
-            'type': type,
-            'source' : source,
-            'paint': 
-            {
-                "circle-color": "red",
-                "circle-blur": 1,
-                "circle-opacity": 0.5,
-                "circle-radius" : {
-                    "property" : "mag",
-                    "stops": [[4, 10], [8, 100]]
-                }
-            }
-        });
+    const features = {
+        "type" : "FeatureCollection",
+	    "name" : "CSV",
+	    "features" : []
+    };
+    let allFeatures = [];
+
+    async function addGeoJsonLayer(id, geoJsonUrl, source, type) {
+        const response = await fetch(geoJsonUrl);
+        const featureCollection = await response.json();
+
+        allFeatures = featureCollection.features.sort((a ,b) => {
+            return a.time < b.time
+        });                
     }
 
     onMount(() => {
@@ -34,9 +27,40 @@
             style: 'https://api.maptiler.com/maps/5a2e698d-bf96-462a-a122-fbd86ebc7aae/style.json?key=r9k1CXRP7YCqI9zWOIjp',
             zoom: 0
         })
-        map.on('load', () => {
-            addGeoJsonLayer("earthquakes_layer", "query_geojson.geojson", "earthquakes", "circle");
-        });
+        map.on('load', async () => {
+            await addGeoJsonLayer("earthquakes_layer", "query_geojson.geojson", "earthquakes", "circle");            
+            
+            for (let feature of allFeatures) {            
+                setInterval(() => {
+                    features.features.push(feature);
+
+                    map.addSource(feature.properties.id, {
+                        type: "geojson",
+                        data: {
+                            "type" : "FeatureCollection",
+                            "name" : "CSV",
+                            "features" : [feature]
+                        }
+                    })
+                    map.addLayer(
+                    {
+                        'id' : feature.properties.id,
+                        'type': 'circle',
+                        'source' : feature.properties.id,
+                        'paint': 
+                        {
+                            "circle-color": "red",
+                            "circle-blur": 1,
+                            "circle-opacity": 0.5,
+                            "circle-radius" : {
+                                "property" : "mag",
+                                "stops": [[4, 10], [8, 100]]
+                            }
+                        }
+                    });
+                }, 200);
+            }                          
+        });          
     })
 
 </script>
