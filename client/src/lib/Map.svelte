e0306a642d4f662abb8d2f09af5695629d1f62f1
lexxamcode <lexamishkin@mail.ru>
2023-04-29 16:55:36 +0400

    Geojson layer added on map

--- client/src/lib/Map.svelte
+++ client/src/lib/Map.svelte
@@ -5,12 +5,29 @@
 
     let map: maplibregl.Map;
 
+    function addGeoJsonLayer(id, data, source, type) {
+        map.addSource(source, {
+          type: "geojson",
+          data: data
+        })
+        map.addLayer(
+        {
+            'id' : id,
+            'type': type,
+            'source' : source,
+            'paint': {"circle-color": "pink"}
+        });
+    }
+
     onMount(() => {
         map = new maplibregl.Map({
             container: 'map',
             style: 'https://api.maptiler.com/maps/5a2e698d-bf96-462a-a122-fbd86ebc7aae/style.json?key=r9k1CXRP7YCqI9zWOIjp',
             zoom: 0
         })
+        map.on('load', () => {
+            addGeoJsonLayer("earthquakes_layer", "query_geojson.geojson", "earthquakes", "circle");
+        });
     })
 
 </script>
