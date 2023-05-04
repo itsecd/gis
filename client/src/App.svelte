bc550ac640752f96a41078d35b750e6b7e76a7cf
lexxamcode <lexamishkin@mail.ru>
2023-05-04 22:09:22 +0400

    Turn on and turn off animation button added

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,17 +1,34 @@
 <script lang="ts">
   import {
     Header,
-    Content
+    Content,
+    SkipToContent,
+    HeaderUtilities,
+    Button
   } from 'carbon-components-svelte';
   import MaplibreMap from './lib/Map.svelte';
+  import Restart from "carbon-icons-svelte/lib/Restart.svelte";
+  import Earthquake from "carbon-icons-svelte/lib/Earthquake.svelte";
+  import 'carbon-components-svelte/css/g100.css';
+
+  let earthquake_rendered: boolean = true;
+  let animation_trigger: boolean = false;
 </script>
 
 <main>
-  <Header>
+  <Header company="ГИИБ">
+    <svelte:fragment slot="skip-to-content">
+      <SkipToContent />
+    </svelte:fragment>
+
+    <HeaderUtilities>
+      <Button kind="ghost" icon={Earthquake} iconDescription="On/Off Animation" tooltipPosition="left" on:click={()=> {animation_trigger = !animation_trigger}}>
+      </Button>
+    </HeaderUtilities>
   </Header>
   
   <Content>
-    <MaplibreMap/>
+    <MaplibreMap  bind:animation_trigger={animation_trigger}/>
   </Content>
 </main>
 
--- client/src/lib/Map.svelte
+++ client/src/lib/Map.svelte
@@ -12,26 +12,6 @@
     };
     let allFeatures = [];
 
-    // Earthquake animation
-    const timer = ms => new Promise(res => setTimeout(res, ms))
-    let earthquake_rendered: boolean = false;
-
-    async function earthquake_animation(ms:number) { 
-        for (let feature of allFeatures) {
-            if (earthquake_rendered)
-                break;
-            
-            console.log(feature);
-
-            features.features.push(feature);
-            (map.getSource('earthquakes') as GeoJSONSource).setData(features);
-
-            await timer(ms); 
-        }
-
-        earthquake_rendered = true;
-    }
-
     async function addGeoJsonLayer(geoJsonUrl) {
         const response = await fetch(geoJsonUrl);
         const featureCollection = await response.json();
@@ -62,6 +42,64 @@
         });
     }
 
+    // Earthquake animation
+    const timer = ms => new Promise(res => setTimeout(res, ms))
+    export let animation_trigger: boolean;
+    let earthquake_rendered: boolean = false;
+    let earthquake_continues: boolean = false;
+
+    async function earthquake_animation(ms:number) {
+        console.log(`cont=${earthquake_continues} setting to true anyway`);
+        console.log(`rend=${earthquake_rendered}`);
+        earthquake_continues = true;
+        let i:number = 0; 
+        for (let feature of allFeatures) {
+            if (earthquake_rendered) {
+                console.log('ANIMATION: already rendered, leaving...')
+                earthquake_continues = false;
+                break;
+            }
+            console.log(i);
+            console.log(feature);
+
+            features.features.push(feature);
+            //@ts-ignore
+            (map.getSource('earthquakes') as GeoJSONSource).setData(features);
+            i++;
+            await timer(ms); 
+        }
+
+        //earthquake_rendered = true;
+        earthquake_continues = false;
+    }
+
+    async function start_earthquake(ms: number) {
+        console.log('vla');
+        if (!earthquake_continues) {
+            console.log(`added geojson_layer`);
+            await addGeoJsonLayer("query_geojson.geojson");
+
+            // Start animation
+            console.log('started animation');
+            await earthquake_animation(ms);
+            earthquake_continues = true;
+        }
+        else {
+            map.removeLayer('earthquakes');
+            map.removeSource('earthquakes');
+            // Clear loaded features
+            allFeatures = [];
+            features.features = [];
+            // Earthquake is not rendered from now
+            console.log('disable');
+            earthquake_rendered = false;
+            earthquake_continues = false;
+        }
+
+        console.log(`earthquake_continues=${earthquake_continues}`);
+        console.log(`earthquake_rendered=${earthquake_rendered}`);
+    }
+    
     onMount(() => {
         map = new maplibregl.Map({
             container: 'map',
@@ -69,13 +107,14 @@
             zoom: 0
         })
         map.on('load', async () => {
-            await addGeoJsonLayer("query_geojson.geojson");            
+            //await addGeoJsonLayer("query_geojson.geojson");            
             console.log(Object.keys(allFeatures).length);   
-            
-            await earthquake_animation(200);                        
+        
+            //await earthquake_animation(200);                        
         });          
     })
 
+    $: animation_trigger, start_earthquake(200);
 </script>
 
 <div id='map'>
